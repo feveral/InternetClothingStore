24df10be54918f2b9aed7bf37a66caf6a1f7e5ef
feveral <feveraly@gmail.com>
2017-11-21 01:00:19 +0800

    change some file name

--- database/InitialDatabase.js
+++ database/InitialDatabase.js
@@ -3,6 +3,6 @@
 require('./CreateTables.js');
 require('./InsertProductBottomWorkingPant.js');
 require('./InsertProductBottomCowBoyShorts.js');
-require('./InsertProductBasicPlainTee.js');
-require('./InsertProductPlainRoundNeckTee.js');
-require('./InsertPlainHatLongTee.js');
+require('./InsertProductTopBasicPlainTee.js');
+require('./InsertProductTopPlainRoundNeckTee.js');
+require('./InsertProductTopPlainHatLongTee.js');
