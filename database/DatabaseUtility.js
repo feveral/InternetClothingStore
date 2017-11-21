ba70fda610e3fd8f3e3e975711575ee3390cd34c
feveral <feveraly@gmail.com>
2017-11-22 01:43:59 +0800

    new product style in index.html

--- database/DatabaseUtility.js
+++ database/DatabaseUtility.js
@@ -5,11 +5,7 @@ const mysql = require('mysql');
 var db = mysql.createConnection({
   	host: "localhost",
   	user: "root",
-<<<<<<< HEAD
-  	password: "timmy880925",
-=======
   	password: "5566",
->>>>>>> 7edb86a3a13a69f18c2ba3ecb5a1658225456cbf
   	database: "Shtick"
 });
 
