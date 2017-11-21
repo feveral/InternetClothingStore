5fa06fabf2b3d8c7fe2f4fb93fa576beca498a7b
LanHow <zzaaq147@gmail.com>
2017-11-21 17:51:39 +0800

    BaseBall Coat

--- database/InsertProductBaseBallCoat.js
+++ database/InsertProductBaseBallCoat.js
@@ -4,7 +4,7 @@ var DatabaseUtility = require('./DatabaseUtility.js');
 var ProductManager = require('../server/ProductManager.js');
 var productManager = new ProductManager(DatabaseUtility.Getdb());
 
-var subCategory = 'Basic'
+var subCategory = 'Coat'
 var category = '棒球外套'
 
 productManager.AddProductIterately(
@@ -20,6 +20,40 @@ productManager.AddProductIterately(
 		ImagePath:''
 	},
 	['深藍','黑色'],
+	['S','M','L','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820027',
+		Name:"美式街頭LOGO棒球外套",
+		Price:1780,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['深藍','黑色'],
+	['S','M','L','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820027',
+		Name:"美式皮袖毛尼棒球外套",
+		Price:1980,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['深藍','暗紅','黑色'],
 	['S','M','L','XL'],
 	DatabaseUtility.callback
 );
