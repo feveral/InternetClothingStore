09fd8829e5776e4d93acdc4c46e7d191876d8a3d
EricKuanJS <john.smith.gj94@gmail.com>
2017-11-23 01:35:50 +0800

    add all the short sleeve

--- database/CreateTables.js
+++ database/CreateTables.js
@@ -2,33 +2,33 @@
 
 var DatabaseUtility = require('./DatabaseUtility.js');
 
-var dropTableProduct = 
+var dropTableProduct =
 	"DROP TABLE IF EXISTS PRODUCT;"
-var createProduct = 
-	"CREATE TABLE PRODUCT" + 
-	 	"(Id INT NOT NULL AUTO_INCREMENT, " + 
+var createProduct =
+	"CREATE TABLE PRODUCT" +
+	 	"(Id INT NOT NULL AUTO_INCREMENT, " +
 	 	"ManagerId INT NOT NULL, " +
-	 	"Name VARCHAR(30) CHARACTER SET utf8 NOT NULL, " + 
-	 	"Price INT ," + 
+	 	"Name VARCHAR(30) CHARACTER SET utf8 NOT NULL, " +
+	 	"Price INT ," +
 	 	"Color VARCHAR(10) CHARACTER SET utf8 NOT NULL," +
-	 	"Size VARCHAR(5) NOT NULL," + 
-	 	"Stock INT ," + 
-	 	"Category VARCHAR(15) CHARACTER SET utf8," + 
-	 	"SubCategory VARCHAR(15) CHARACTER SET utf8," + 
+	 	"Size VARCHAR(5) NOT NULL," +
+	 	"Stock INT ," +
+	 	"Category VARCHAR(15) CHARACTER SET utf8," +
+	 	"SubCategory VARCHAR(15) CHARACTER SET utf8," +
 	 	"ImagePath VARCHAR(255) CHARACTER SET utf8 ," +
-	    "PRIMARY KEY (Id)" + 
+	    "PRIMARY KEY (Id)" +
 	");";
 
-var createMember = 
-	"CREATE TABLE MEMBER" + 
+var createMember =
+	"CREATE TABLE MEMBER" +
 		"(Id INT NOT NULL AUTO_INCREMENT , " +
 		"Name VARCHAR(50) CHARACTER SET utf8 NOT NULL , " +
 		"MemberType , " +
-		"Email VARCHAR(255) NOT NULL , " + 
-		"Cellphone CHAR(10) NOT NULL ," + 
+		"Email VARCHAR(255) NOT NULL , " +
+		"Cellphone CHAR(10) NOT NULL ," +
 		"Password VARCHAR(255) NOT NULL , " +
 		"Address VARCHAR(255) CHARACTER SET utf8 ,"+
-		"PRIMARY KEY (Id)" + 
+		"PRIMARY KEY (Id)" +
 	");";
 
 DatabaseUtility.ExecuteSQLCommand(dropTableProduct);
--- database/InsertProductTopShortSleeve.js
+++ database/InsertProductTopShortSleeve.js
@@ -4,7 +4,7 @@ var DatabaseUtility = require('./DatabaseUtility.js');
 var ProductManager = require('../server/ProductManager.js');
 var productManager = new ProductManager(DatabaseUtility.Getdb());
 
-var subCategory = 'Basic'
+var subCategory = 'Top'
 var category = '短袖上衣'
 
 productManager.AddProductIterately(
@@ -789,5 +789,1007 @@ productManager.AddProductIterately(
 	},
 	['灰色','黑色'],
 	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式燈塔文字短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['紅色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式迷彩戰鬥機短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色'],
+	['S'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"加州夕陽LOGO短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"品牌LOGO字母圓弧短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['緞彩黑'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"街頭品牌LOGO字母短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['緞彩黑','緞彩灰','粉紅','深灰'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式小狗字母短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','粉橘'],
+	['M','L','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式數字框線短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','粉橘'],
+	['M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式MARINE_LIFE帆船短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','白色','綠色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式老鷹字母短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['綠色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"WORLD_PEACE_LOVE笑臉短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','藍色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式PEACE老鷹短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['粉橘','黑色'],
+	['S','M','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"SOUL_POWER字母短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['綠色','黑色'],
+	['S','M','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式FFAR字母短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','黑色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"街頭迷彩LOGO短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色'],
+	['S'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式NY錯視撞色短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','灰色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式國旗變形蟲短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"SLSCO撞色字母短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','藍色','黑色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"STLS城市字母短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['藍色'],
+	['S'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"BIKE線條簡約腳踏車短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','黑色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"棒球帽鬥牛犬短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','黑色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"LOBBY_BOY大堂男孩短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','黑色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"戴帽的鬍子叔叔短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','黑色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"撞色線條描臉短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','黑色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"6眼部落面具短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','黑色'],
+	['S','M','L','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"6眼部落面具短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','黑色'],
+	['S','M','L','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"POTATO薯條短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','白色'],
+	['S','M','L','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"DOGS_OF_WAR戰爭犬短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','白色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"DONT_WEAR_BRAS文字短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','白色'],
+	['S','M','L','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"電眼潮流吉娃娃短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','白色'],
+	['M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"BETTER_CHOICE文字短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','灰色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"SLAB品牌變形蟲文字短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','白色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"SLAB品牌變形蟲文字短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"品牌文字撕標短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','深藍'],
+	['XS'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"殺人鯨設計短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','黑色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"街頭手掌8短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','黑色'],
+	['S','M','L','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式握手文字短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','黑色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"DO_YOU_LOVE_ME文字短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','黑色'],
+	['M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"STILLAS_發火警車短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"品牌變形蟲LOGO短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','藍色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"品牌植絨旗幟短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"品牌美式老鷹短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','白色','綠色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"品牌EST1994老鷹短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['綠色'],
+	['S'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"STILLAS_個性字形短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['藍色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"STILLAS_SPORTS短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['藍色'],
+	['S','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"品牌潑漆文字短TEE",
+		Price:480,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','黑色'],
+	['S'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"品牌LOGO拼接撞色POLO衫",
+		Price:580,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['藍白紅','黑紅綠'],
+	['M','L','XL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式條紋撞色短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['深藍','白綠'],
+	['XS','M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"媽媽給我錢_中文印花短T",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','白色'],
+	['XS'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"下擺圓弧接袖短TEE",
+		Price:490,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['深灰','青綠'],
+	['M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"USA國旗色印花短T",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['紅色'],
+	['M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"美式拼接圖騰短T",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白黑'],
+	['M'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"品牌字母線條短T",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['紅色'],
+	['XS'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"滿版字母C街頭短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色','白色','灰色'],
+	['XS','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"LIEF圖騰休閒短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色'],
+	['XS'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"SAS紋路拼接圓領短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰黑','白紅','白黑'],
+	['M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"SHTICK_高磅素色V領短TEE",
+		Price:290,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['白色','深灰色','深藍色','麻灰藍','麻花黑','麻花藍','黑色','緞彩灰','緞彩黑'],
+	['XS','S','M','L','XL','XXL'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"大麻葉棒球短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['灰色','黑色'],
+	['S','M','L'],
+	DatabaseUtility.callback
+);
+
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"STILLAS緞帶潮流設計短TEE",
+		Price:380,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['黑色'],
+	['S'],
+	DatabaseUtility.callback
+);
+productManager.AddProductIterately(
+	{
+		ManagerId:'104820021',
+		Name:"SHTICK_高磅素色圓領短TEE",
+		Price:290,
+		Color:'',
+		Size:'',
+		Stock:0,
+		Category: subCategory,
+		SubCategory: category,
+		ImagePath:''
+	},
+	['土黃','天藍','水藍','卡其','白色','灰白條','灰藍','米黃','杏色','咖啡','亮藍','紅色','軍綠','粉紅','粉紫','粉綠','粉藍','酒紅','深灰','深藍','莓紅','麻花白','麻花黑','麻花藍','湖藍','紫藍','黑白條','黑色','綠色','緞彩灰','緞彩黑','橘紅','藍白條','寶藍','鐵灰'],
+	['XS','S','M','L','XL','2XL','3XL','4XL','5XL'],
 	DatabaseUtility.callback
 );
