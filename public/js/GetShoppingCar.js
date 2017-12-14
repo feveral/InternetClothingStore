d25f55332fea9850f6b82f85fc48f0f7ed40a608
feveral <feveraly@gmail.com>
2017-12-15 03:12:19 +0800

    Add a shoppingCar API

--- public/js/GetShoppingCar.js
+++ public/js/GetShoppingCar.js
@@ -1,8 +1,23 @@
 
 $(document).ready(function(){
+	InitialShoppingCarHover();
+});
+
+
+function InitialShoppingCarHover(){
+
 	$('#member > a').hover(ShowShoppingCarHeader,HideShoppingCarHeader);
 	$('#shoppingCar').hover(ShowShoppingCarHeader,HideShoppingCarHeader);
-});
+
+	var apiUrl = GetServerUrl() + '/shoppingCar';
+	var callback = RenderShoppingCarHover;
+	AjaxGet(apiUrl,callback);
+}
+
+function RenderShoppingCarHover(msg){
+	var object = JSON.parse(msg);
+	var data = object['data'];
+}
 
 function ShowShoppingCarHeader(){
 	$('#shoppingCar').css('height','450px');
